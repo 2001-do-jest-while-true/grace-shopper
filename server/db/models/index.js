8d2c4127b4b1cfcee64dc15b487de0b5e4f95ad5
Karen <k.ortiz026@gmail.com>
2020-03-02 21:36:12 -0500

    defined associations, could not test yet

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user')
+const Product = require('./product')
+const Order = require('./order')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +9,10 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+User.hasMany(Order);
+
+Product.belongsToMany(Order, {through:"order_product"});
+Order.belongsToMany(Product, {through:"order_product"});
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +20,7 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Product,
+  Order
 }
