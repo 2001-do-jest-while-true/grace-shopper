dbd853063e805a9cb0c8ecbe94fdfd6b9f89655c
Lauren Hash <lah5175@hotmail.com>
2020-03-03 16:12:00 -0500

    Fixed bugs with SingleProduct component

--- client/routes.js
+++ client/routes.js
@@ -22,11 +22,11 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/products/:productId" component={SingleProduct} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
-            <Route path="/products/:productId" component={SingleProduct} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
