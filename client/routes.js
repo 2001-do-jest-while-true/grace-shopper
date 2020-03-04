9d0f39fa7aabe0c778bb3e016962a31920ca0f9c
Lauren Hash <lah5175@hotmail.com>
2020-03-04 09:41:53 -0500

    Fixed /home for logged in users

--- client/routes.js
+++ client/routes.js
@@ -19,6 +19,7 @@ class Routes extends Component {
 
   render() {
     const {isLoggedIn} = this.props
+    console.log(isLoggedIn)
 
     return (
       <Switch>
@@ -49,7 +50,7 @@ const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.loggedIn.id
   }
 }
 
