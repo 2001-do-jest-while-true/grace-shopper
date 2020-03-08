1809b55a073018976f6eddd8e490aae15a06b84c
Lauren Hash <lah5175@hotmail.com>
2020-03-08 16:01:52 -0400

    Added checkout to the cart

--- client/routes.js
+++ client/routes.js
@@ -14,6 +14,7 @@ import EditProduct from './components/editProduct'
 import {initializeCartThunk, fetchCart, setCart} from './store/cart'
 import AdminUser from './components/adminUser'
 import UserSignup from './components/UserSignup'
+import OrderConfirmation from './components/orderConfirmation'
 
 let cartFlag = false
 
@@ -26,7 +27,6 @@ class Routes extends Component {
     this.props.fetchAllProducts()
 
     const cartStr = window.localStorage.getItem('cart')
-    console.log(cartStr)
     this.props.setCart(JSON.parse(cartStr))
 
     window.addEventListener('beforeunload', async event => {
@@ -61,6 +61,7 @@ class Routes extends Component {
         <Route exact path="/products" component={AllProducts} />
         <Route exact path="/products/:productId" component={SingleProduct} />
         <Route exact path="/cart" component={Cart} />
+        <Route path="/cart/checkout" component={OrderConfirmation} />
         {/* <Route exact path="/users" component={allUsers} />
         <Route path="/users/:userId" component={SingleUser} /> */}
 
